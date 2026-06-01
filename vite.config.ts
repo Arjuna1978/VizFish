48569094d99d3ee80c5a56cb12982eb8cbb17b4f
Arjuna Vijayanayagam <99155024+Arjuna1978@users.noreply.github.com>
2026-06-01 10:42:19 +1000

    Fix formatting in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
@@ -7,5 +7,5 @@ export default defineConfig({
   base: '/VizFish/',
   server: {
     port: 3000,
-  }
+  },
 })
